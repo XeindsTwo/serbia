798731da4bc8d2d27aa5e24c2e365214a79fc5ab
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-12-11 21:35:47 +0300

    adaptive header

--- app/js/mobileMenu.js
+++ app/js/mobileMenu.js
@@ -1,28 +1,51 @@
 export function setupMobileMenu() {
   const html = document.documentElement;
+  const body = document.body;
   const menuBtn = document.querySelector('.menu-btn');
   const headerMobile = document.querySelector('.header__mobile');
-  const headerLogo = document.querySelector('.header__logo');
+  const headerInner = document.querySelector('.header__inner');
   const anchors = document.querySelectorAll('a.mobile');
+  const allInteractiveElements = document.querySelectorAll('a, button, input, select, textarea');
+
+  function updateTabIndex(isMenuActive) {
+    for (const element of allInteractiveElements) {
+      if (headerMobile.contains(element) || headerInner.contains(element)) {
+        element.tabIndex = isMenuActive ? 1 : 0;
+      } else {
+        element.tabIndex = isMenuActive ? -1 : 0;
+      }
+    }
+  }
 
   menuBtn.addEventListener('click', () => {
     menuBtn.blur();
-    html.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-    headerMobile.classList.toggle('active');
-    headerLogo.classList.toggle('active');
+    const isMenuActive = !html.classList.contains('active');
+    html.classList.toggle('active', isMenuActive);
+    body.classList.toggle('active', isMenuActive);
+    menuBtn.classList.toggle('active', isMenuActive);
+    headerMobile.classList.toggle('active', isMenuActive);
+    updateTabIndex(isMenuActive);
   });
 
   function scrollToTarget(targetId) {
     const targetSection = document.querySelector(targetId);
     if (targetSection) {
       html.classList.remove('active');
+      body.classList.remove('active');
       headerMobile.classList.remove('active');
       menuBtn.classList.remove('active');
-      headerLogo.classList.remove('active');
+      updateTabIndex(false);
+
       setTimeout(() => {
-        const targetOffset = targetSection.offsetTop - 25;
-        window.scrollTo({top: targetOffset, behavior: 'smooth'});
+        let targetOffset;
+
+        if (targetId === '#serbia') {
+          targetOffset = targetSection.getBoundingClientRect().top + window.pageYOffset;
+        } else {
+          targetOffset = targetSection.offsetTop - 34;
+        }
+
+        window.scrollTo({ top: targetOffset, behavior: 'smooth' });
       }, 700);
     }
   }
